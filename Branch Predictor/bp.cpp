9ee394f9371fcd451eed479f56a5bb416e2583b4
BatelOved <bateloved1@gmail.com>
2022-11-28 11:34:55 +0200

    bug fix & updated tests

--- Branch Predictor/bp.cpp
+++ Branch Predictor/bp.cpp
@@ -246,7 +246,7 @@ void BTB::insertBranch(uint32_t pc, uint32_t targetPc, bool taken) {
 		targetBranch->hist = local_hist;
 		targetBranch->targetPc = targetPc;
 		targetBranch->fsmTable = make_shared<FSM_Table>(pow(2,historySize), fsmState);
-		targetBranch->fsmTable->update(histToInt(targetBranch->hist), taken);
+		targetBranch->fsmTable->update(fsmEntry, taken);
 		targetBranch->updateHist(taken);
 	}
 
